6471685a76bbd1b8fa66712d4c52b1ce8e6fcb9a
Sagar Adhikari <sageradh@gmail.com>
2024-04-19 10:07:07 -0400

    Added Dynamic Port and Host

--- run.py
+++ run.py
@@ -1,13 +1,15 @@
 from app import create_app
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 # app, celery_app = create_app()
 # app.app_context().push()
 
-
+host = os.getenv("HOST", "127.0.0.1")
+port = int(os.getenv("PORT", 5000))
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(use_reloader=False)
+    app.run(host=host, port=port, use_reloader=False)
 #     celery.start()
