e9256893297f60b97dc737bf6a972619e2a73079
Sagar Adhikari <sageradh@gmail.com>
2024-04-19 15:58:51 -0400

    Coding cleaning

--- run.py
+++ run.py
@@ -3,11 +3,9 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-# app, celery_app = create_app()
-# app.app_context().push()
 
 host = os.getenv("HOST", "127.0.0.1")
-port = int(os.getenv("PORT", 5000))
+port = int(os.getenv("PORT", 8081))
 app = create_app()
 
 if __name__ == '__main__':
