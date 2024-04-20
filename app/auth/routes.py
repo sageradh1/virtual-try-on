298139f3f8a1c7475e7cdacf72cabe24d9ee7b96
Sagar Adhikari <sageradh@gmail.com>
2024-04-19 20:49:26 -0400

    Get generated images

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -184,22 +184,22 @@ def get_users():
 @auth.route('/get-generated-images', methods=['GET'])
 def get_generated_images():
     try:
-        if 'username' not in session:
+        username=request.args.get('username')
+        if not username:
             return jsonify({
-                "status": 401,
-                "error": "Unauthorized",
-                "message": "Need to login before accessing your product id"
-            }), 401
-        username = session['username']
+                    "status": 422,
+                    "error": "Bad request",
+                    "message": f"Username is missing."
+                }), 422
         images = GeneratedImage.query.filter_by(username=username)
         images_data = [image.to_dict() for image in images]
         return jsonify(images_data), 200
     except Exception as e:
-            app_logger.exception("Error while fetching generated images", e)
-            return jsonify({
-                "status": 500,
-                "message": "Error while fetching information"
-            }), 400
+        app_logger.exception("Error while fetching generated images", e)
+        return jsonify({
+            "status": 500,
+            "message": "Error while fetching information"
+        }), 400
 
 def filter_by_gender(list, gender):
     filtered_list = [obj for obj in list if obj.get("gender") == gender]
